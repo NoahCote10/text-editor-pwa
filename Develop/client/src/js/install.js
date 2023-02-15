1454210cc73b961adf4454508c2bb5fbffb1932f
Noah Cote <noahcote10@gmail.com>
2023-02-15 17:59:18 -0500

    added logic for put and get methods. added logic for installing pwa. started working on asset caching. code still needs to be tested.

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,33 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // store triggered event
+    window.deferredPrompt = event;
+
+    // unhides the install button
+    butInstall.classList.toggle('hiddern', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+     return;
+    }
+  
+    // Shows prompt
+    promptEvent.prompt();
+    
+    // Resets the deferred prompt variable
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // clears prompt
+    window.deferredPrompt = null;
+});
